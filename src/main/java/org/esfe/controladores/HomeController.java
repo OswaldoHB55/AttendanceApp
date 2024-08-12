f36f2afee85d3a46152c9294d58cd801388b3173
OswaldoHB55 <nestor.72607424@gmail.com>
2024-08-12 11:06:47 -0600

    Implementacion de Sprint Security

--- src/main/java/org/esfe/controladores/HomeController.java
+++ src/main/java/org/esfe/controladores/HomeController.java
@@ -1,6 +1,11 @@
 package org.esfe.controladores;
 
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +13,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
     @GetMapping
-    public String index(){
+    public String index(Model model){
+        // Obtener la autenticación actual
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+        // Verificar si el usuario está autenticado
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            String username;
+
+            // Si el principal es una instancia de UserDetails, obtenemos el nombre de usuario
+            if (principal instanceof UserDetails) {
+                username = ((UserDetails) principal).getUsername();
+            } else {
+                // Si no, simplemente obtenemos el nombre
+                username = principal.toString();
+            }
+
+            // Pasar el nombre de usuario al modelo
+            model.addAttribute("username", username);
+        } else {
+            model.addAttribute("username", "invitado"); // Si no está autenticado
+        }
         return "home/index";
     }
 }
